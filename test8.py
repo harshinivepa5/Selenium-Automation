3e09612b56011f47d3cf0521c2f6934392017d8a
Surya Sriharshini Vepa <149376349+harshinivepa5@users.noreply.github.com>
2024-06-02 16:32:03 +0530

    Update test8.py

--- test8.py
+++ test8.py
@@ -9,7 +9,7 @@ driver.get("https://dreams.ntpc.co.in/Account/Login?ReturnUrl=%2F")
 username_input = driver.find_element(By.ID,"Email")
 password_input = driver.find_element(By.ID,"Password")
 username_input.send_keys("1")
-password_input.send_keys("1")
+password_input.send_keys("1") #actual login crentials are not given for privacy purpose.
 time.sleep(15)
 login_button = driver.find_element(By.XPATH,'//*[@id="con"]/div/div/div[2]/div[3]/div[2]/form/div[4]')
 login_button.click()
